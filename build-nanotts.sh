1d208e29ffdcfc19a91516fdcbc1721111ecb084
Andre Miras <andre.miras@gmail.com>
2016-11-01 00:26:44 +0100

    Update build-nanotts.sh various fixes
     * Fixes broken paths
     * Deploys to /app/.apt/usr/bin/ rather than /app/.heroku/python/bin/

--- build-nanotts.sh
+++ build-nanotts.sh
@@ -1,14 +1,16 @@
 # builds nanotts for Heroku and upload it to transfer.sh
-cd /tmp/
-wget -c 'https://github.com/gmn/nanotts/archive/master.zip'
-unzip master.zip
+wget -c 'https://github.com/gmn/nanotts/archive/master.zip' -P /tmp/
+unzip /tmp/master.zip -d /tmp/
+mv Makefile.patch /tmp/nanotts-master/
 cd /tmp/nanotts-master/
 patch < Makefile.patch
 make
 # prepares the directory structure so that
 # heroku-buildpack-vendorbinaries extracts it to:
-# /app/.heroku/python/bin/
-# which is already part of the PATH
-mkdir -p .heroku/python/bin/
-mv nanotts .heroku/python/bin/
-curl --upload-file ./nanotts.tar.gz https://transfer.sh/
+# /app/.apt/usr/bin/
+# which is already part of the PATH, probably thanks to
+# heroku-buildpack-apt
+mkdir -p .apt/usr/bin/
+mv nanotts .apt/usr/bin/
+tar -cvzf nanotts.tar.gz .apt/
+curl --upload-file nanotts.tar.gz https://transfer.sh/
